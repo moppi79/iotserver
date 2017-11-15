9f33f93e364d49036dbb3e880133d5e5dd04d173
Moppi <33616102+moppi79@users.noreply.github.com>
2017-11-15 12:31:25 +0100

    Create demosensor.py

---
+++ sensors/demosensor.py
@@ -0,0 +1,8 @@
+import daemon, os, time, sys, signal, lockfile, daemon.pidfile, socket, logging, datetime, json, random
+
+class demo_sensor():
+	
+	def out(self,adresse,speicher):
+		
+		ret = {'temp':'22:5', 'feucht':'56,4'}
+		return(ret)
