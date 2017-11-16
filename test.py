18a850333e476ed7f72199af25b596df5c20b346
moppi79 <mooppi79@googlemail.com>
2017-11-16 09:01:17 +0100

    inhalt neu

--- test.py
+++ test.py
@@ -1,1 +1,7 @@
-sss
+git commit <-- welche dateien (zum ankucken)
+
+git add (datei/verzeichniss) zum Übertragen eintragen
+
+git commit <-- öffenet sich datei es müssen die # entfernt werden
+
+git push <-- zum hoch laden fertig machen
