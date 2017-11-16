64639ee044177b672ff26f51057db451b334c6ae
moppi79 <mooppi79@googlemail.com>
2017-11-16 09:13:24 +0100

    geändert:       iotserver.py / inhalt kopiert 	geändert:       serverclient.py /inhalt kopiert

--- serverclient.py
+++ serverclient.py
@@ -1,11 +1,11 @@
-import daemon, os, time, sys, signal, lockfile, daemon.pidfile, socket, logging, datetime, json, random
+cimport daemon, os, time, sys, signal, lockfile, daemon.pidfile, socket, logging, datetime, json, random
 
 from collections import defaultdict
 from i2cdriver import i2c_treiber
 #from i2ccall import i2c_abruf
 
 from sensors.demosensor import demo_sensor
-
+from module.mcp23017 import mcp23017
 
 logger = logging.getLogger()
 logger.setLevel(logging.ERROR)
@@ -44,7 +44,9 @@ ic_list {
 
 ic_chip = defaultdict(object)
 
-ic_chip[1] ={'num':6,#anzahl ports
+ic_chip[1] ={'icname':'mcp23017',
+			'adresse':0x20,
+			'num':6,#anzahl ports
 			'bank':2,#anzahl banken
 			'pins':8,#anzahlpins
 			100:[0x00,0x00,0x12], #adresse der bank, start wert, export register MCP23017
@@ -57,6 +59,7 @@ ic_chip[1] ={'num':6,#anzahl ports
 			5:[0x12,0x04,'heizung','Heizung','0'],#IRLZ schalter - heizung für regensensor
 			6:[0x12,0x08,'Heartbeat','led','0'],#LED heartbeat
 			}
+			
 sensordic = defaultdict(object)		
 			
 sensordic = {1:[0x99,'options',demo_sensor(),'temperatur/feuchtigkeit'],
@@ -66,6 +69,7 @@ sensordic = {1:[0x99,'options',demo_sensor(),'temperatur/feuchtigkeit'],
 ram = defaultdict(object)
 ram = {}
 
+ic_class = {'mcp23017':mcp23017()}
 
 class i2c_abruf:
 	
@@ -73,107 +77,11 @@ class i2c_abruf:
 		if 'firstrun' in ram:
 			ram['firstrun'] = 1 #platzhalter
 		else:
-			ram['firstrun'] = 1
-			looplist = 0 #ic loop
-			loopbank = 0 #register bank Loop
-			loopchip = 0 #ic Pin Loop
-								
-			while looplist < ic_list['num']:
-				looplist += 1
-
-				ic2 = i2c_treiber(ic_list[looplist]) #verbfung zum IC aufbauen
+			for x in ic_chip:
+				classcall = ic_class[ic_chip[x]['icname']]
+				print (classcall.install('data'))
 				
-				#bank while für start werte install
-				while loopbank < ic_chip[looplist]['bank']:
-					bankcount = 100 + loopbank
-					wert = (self.ramlokation(ic_list[looplist], ic_chip[looplist][bankcount][0], bankcount))#wert im speicher IC adresse, Bank programmier adresse, bank nummer im ic_chip array
-					ram.update({wert:ic_chip[looplist][bankcount][1]})
-					wert = (self.ramlokation(ic_list[looplist], ic_chip[looplist][bankcount][2], 'value')) #der immer zu sehende Bank wert beim auslesen
-					ram.update({wert:0})
-					loopbank += 1
-					ic2.write(ic_chip[looplist][bankcount][0],ic_chip[looplist][bankcount][1]) #IC speicher schreiben
-					loopblank = 0
-					blankintern = 1
-			
-					while loopblank < ic_chip[looplist]['pins']:#Für jeden pin in der bank einen blanko array punkt erstellen
-						wert = (self.ramlokation(ic_list[looplist], blankintern, ic_chip[looplist][bankcount][2]))#IC adresse, Pin, Bank
-						ram.update({wert:0}) #soll wert, wenn anders muss funktion ausgeführt werden
-						chache = wert + 'chache' #zwischenspeicher für einzel funktionen,
-						ram.update({chache:0})
-						funktion = wert + 'funktion'
-						ram.update({funktion:0}) #hier array adresse für funktion
-						blankintern = blankintern * 2
-						loopblank += 1
-
-				while loopchip < ic_chip[looplist]['num']:
-					wert=0
-					#print (ic_chip[looplist][1]
-					loopchip += 1 
-					wert = (self.ramlokation(ic_list[looplist], ic_chip[looplist][loopchip][1], ic_chip[looplist][loopchip][0])) #wert im speicher: IC adresse, Pin speicher wert, bank ausgabe
-					ram[wert]= loopchip #hinterlegte funktion im array 
-					
-	
-				loopchip = 0 #reset vom IC-Pin loop
-				ic2.close() #verbindung schliessen 			
-
-	
-	def ramlokation (self, slaveadress, dictorylokation, bank): #zum berechnen der postion im ram jedes einzelen aktoren
-		return str(slaveadress) + str(dictorylokation) + str(bank)
-		
-	def integertobyte (self,wert): #den Hex wert aus der bank in einen 8 bit array umschreiben
-		array = [i for i in range(9)]
-		count = 128;
-		count2 = 8
-		while count2 > 0:
-			new = wert - count
 				
-			if new < 0:
-				array[count2] = 0			
-			elif new == 0:
-				wert = 0
-				array[count2] = 1
-			else:
-				array[count2] = 1
-				wert = new
-			
-			count = count / 2
-			count2 -= 1
-		return(array)		
-
-	def abgleich (self): # fragt die bänke ab und vergleicht es mit dem Ram abbild um veränderungen festzustellen
-		looplist = 0 #ic loop
-		loopchip = 0 #ic Pin Loop
-		
-		while looplist < ic_list['num']:
-			looplist += 1
-			
-			ic2 = i2c_treiber(ic_list[looplist]) 
-			while loopchip < ic_chip[looplist]['bank']:
-				bank = 100 + loopchip
-				loopchip += 1
-				data = ic2.read(ic_chip[looplist][bank][2])
-				
-				ramobjekt = self.ramlokation(ic_list[looplist], ic_chip[looplist][bank][2], 'value')
-				if data[1] != ram[ramobjekt]:
-					ram[ramobjekt] = data[1]
-					loopchip2 = 1
-					bitcharge = 128
-					chip_array = self.integertobyte(data[1]) 
-					while loopchip2 < 8:
-						 
-						wert = self.ramlokation(ic_list[looplist], bitcharge, ic_chip[looplist][bank][2])
-						bitcharge = bitcharge / 2 
-						loopchip2 += 1
-						'''
-						#hier dann die einzelenen funktionen abrufen
-						#muss noch programmiert und getestet werden
-						'''
-
-					print (chip_array)
-			
-			ic2.close()
-			
-			
 class server_coneckt:
 	
 	def __init__(self):
@@ -267,25 +175,14 @@ class server_coneckt:
 def main_loop():
 	loopmaster = 0
 	while True:
-		
-		
-		
+
 		ramm = i2c_abruf()
 		
 		tester = server_coneckt()
 		
-		
 		tester.check()
 		
-		
-		
-		#print (tester.timeslice())
-		
-		#print(ram)	
-
-		#tester.check()
-		
-		#print (ram['sensor'])
+		print(ram)
 		
 		if ram['sensor'] != 0:
 			data = {'funktion':'sensor','sensor':'start', 'target_key':ram['sensor'], 'name':ic_list['name'], 'host':ic_list['host']}
@@ -297,10 +194,10 @@ def main_loop():
 					
 					deliver = {'funktion':'sensor','sensor':'deliver', 'target_key':ram['sensor'], 'name':ic_list['name'], 'host':ic_list['host']}
 					deliver['werte'] = {}
-					'''
-					sensordic = {1:[0x99,demo_sensor(),'name','temperatur/feuchtigkeit'],
-								2:[0xa0,demo_sensor(),'name','licht'],
-					'''
+					
+					#sensordic = {1:[0x99,demo_sensor(),'name','temperatur/feuchtigkeit'],
+					#		#	2:[0xa0,demo_sensor(),'name','licht'],
+					
 					
 					for x in sensordic:
 						deliver['werte'][x] = {}
@@ -316,22 +213,6 @@ def main_loop():
 					time.sleep(0.05)
 				
 		
-
-		#print (test.timeslice())
-		
-		
-		'''
-		test = timer_san()
-		test.new_client('mops')
-		test.new_client('mops3')
-		#print (len(variable))
-		test.timeslicer()
-		print(variable)
-		print(timeschlitz)
-		'''
-		
-		##siehe tester.py für mehr alten kram
-		
 		#print (loopmaster)
 		time.sleep(0.1)
 		loopmaster += 1
@@ -341,7 +222,11 @@ def main_loop():
 			antwort = json.loads(tester.sock(json.dumps(testersa)))
 			
 			break #zum solo testen muss am schluss entfernt werden
-		#break #muss zum ende entfernt werden
+		
+		testersa = {'funktion':'delete','name':ic_list['name'] ,'host':ic_list['host']}
+		
+		antwort = json.loads(tester.sock(json.dumps(testersa)))
+		break #muss zum ende entfernt werden
 
 
 
